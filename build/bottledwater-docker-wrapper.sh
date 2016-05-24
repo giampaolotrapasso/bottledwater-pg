bccf49b9956eca550a477a92c720d1eaaaa575f6
Giampaolo Trapasso <giampaolo.trapasso@gmail.com>
2016-05-24 14:25:40 +0100

    changes to make composition work

--- build/bottledwater-docker-wrapper.sh
+++ build/bottledwater-docker-wrapper.sh
@@ -1,6 +1,34 @@
 #!/bin/sh
 
+if [ -z "$POSTGRES_PORT_5432_TCP_ADDR" ]; then
+	POSTGRES_PORT_5432_TCP_ADDR=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' bottledwaterpg_postgres_1)
+fi
+
+if [ -z "$POSTGRES_PORT_5432_TCP_PORT" ]; then
+	POSTGRES_PORT_5432_TCP_PORT=5432
+fi
+
+
+if [ -z "$KAFKA_PORT_9092_TCP_ADDR" ]; then
+	KAFKA_PORT_9092_TCP_ADDR=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' bottledwaterpg_kafka_1)
+fi
+
+if [ -z "$KAFKA_PORT_9092_TCP_PORT" ]; then
+	KAFKA_PORT_9092_TCP_PORT=9092
+fi
+
+if [ -z "$SCHEMA_REGISTRY_PORT_8081_TCP_ADDR" ]; then
+	SCHEMA_REGISTRY_PORT_8081_TCP_ADDR=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' bottledwaterpg_schema-registry_1)
+fi
+
+if [ -z "$SCHEMA_REGISTRY_PORT_8081_TCP_PORT" ]; then
+	SCHEMA_REGISTRY_PORT_8081_TCP_PORT=8081
+fi
+
+
+
 POSTGRES_CONNECTION_STRING="hostaddr=$POSTGRES_PORT_5432_TCP_ADDR port=$POSTGRES_PORT_5432_TCP_PORT dbname=postgres user=postgres"
+
 KAFKA_BROKER="$KAFKA_PORT_9092_TCP_ADDR:$KAFKA_PORT_9092_TCP_PORT"
 
 if [ -n "$SCHEMA_REGISTRY_PORT_8081_TCP_ADDR" ]; then
@@ -16,4 +44,5 @@ exec /usr/local/bin/bottledwater \
     --broker="$KAFKA_BROKER" \
     $schema_registry_opts \
     "$@"
+
 
